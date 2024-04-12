da05d3ac3d18c75d26a121546007a7f3beb33ff3
anand kumar <ankm335@gmail.com>
2024-04-12 12:04:54 +0530

    ui: fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-
+import React from "react";
+import axios from "axios";
 function App() {
+  const [data, setData] = React.useState({ id: "", name: "" });
+  const [list, setList] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      let fetch = axios.get("http://3.109.32.218:4545/all-names");
+      console.log("->", (await fetch).data());
+    };
+    fetchData();
+  }, []);
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="">
+        <label htmlFor="id">Pic Id</label>
+        <input type="text" name="id" id="id" placeholder="Pic Id" />
+      </div>
+      <div>
+        <label htmlFor="name">Pic Name</label>
+        <input type="text" name="name" id="name" placeholder="Pic Name" />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
